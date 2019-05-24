f7edae373fb9bcbe36225545519d91beb66ddd26
ahmetonurslmz <ahmetonurslmz@gmail.com>
2019-05-24 11:43:20 +0300

    MarkCompleteFunction

--- client/src/components/task.vue
+++ client/src/components/task.vue
@@ -2,7 +2,8 @@
     <div>
         <b-list-group-item>
             <b-row>
-              <b-col cols="10">{{task.text}}</b-col> 
+              <b-col cols="2"> <b-form-checkbox type="checkbox" @change="markComplete"></b-form-checkbox></b-col>
+              <b-col cols="8" v-bind:class="{'task-text-completed': task.state }">{{task.text}}</b-col> 
               <b-col cols="2"><b-button pill variant="outline-danger" @click="$emit('del-task', task.id)" size="sm">X</b-button></b-col>
             </b-row>
         </b-list-group-item>
@@ -11,7 +12,17 @@
 <script>
 export default {
     name: 'Task',
-    props:["task"]
+    props:["task"],
+    methods: {
+        markComplete() {
+            this.task.state=!this.task.state
+        }
+    }
 }
 </script>
 
+<style scoped>
+.task-text-completed {
+    text-decoration: line-through;
+}
+</style>
